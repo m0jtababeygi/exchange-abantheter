2de515238b1a4755cd2e43d59acf04a1ae038567
m.beygi <M.Beygi@sadad.co.ir>
2025-06-04 15:14:41 +0330

    desctop  menu

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -23,9 +23,9 @@ const Header = () => {
       <div className={classes.headerMobile_container}>
         <div className={classes.headerMobile_right}>
           {/* منو آیکون */}
-          <div className={`${classes.headerMobile_menu} md:hidden`}>
+          <div className={`${classes.headerMobile_menu} lg:hidden`}>
             <span
-              className="cursor-pointer block md:hidden"
+              className="cursor-pointer block lg:hidden"
               onClick={() => setMenuOpen(true)}
             >
               <FontAwesomeIcon
@@ -36,7 +36,7 @@ const Header = () => {
           </div>
 
           {/* لوگو */}
-          <div className={classes.headerMobile_logo}>
+          <div className={`${classes.headerMobile_logo} lg:hidden`} >
             <a href="" className="h-full w-full cursor-pointer">
               <Image
                 src="/img/primary-icon.svg"
@@ -46,36 +46,46 @@ const Header = () => {
               />
             </a>
           </div>
-        </div>
+          <div className={`${classes.headerDesktop_logo} relative h-20 w-56 hidden lg:block`}>
+            <a href="" className="h-full w-full cursor-pointer">
+              <Image
+                src="/img/main-persian.svg"
+                alt="header desktop logo"
+                fill
+                className="object-contain"
+              />
+            </a>
+          </div>
+        </div> 
 
-        <nav className="hidden md:flex gap-6 items-center text-sm">
-          <a href="#" className="text-gray-800 text-lg">
+        <nav className="hidden lg:flex items-center justify-center gap-[2.5rem] text-sm">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             قیمت لحظه‌ای
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             معامله فوری
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             اعتبار معاملاتی
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             اعتبار خرید کالا
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             بلاگ
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700">
             سوالات متداول
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700 hidden 2xl:inline">
             قوانین
           </a>
-          <a href="#" className="text-gray-800 text-base">
+          <a href="#" className="text-gray-800 text-base hover:text-blue-700 hidden 2xl:inline">
             تماس با ما
           </a>
         </nav>
 
-        <div className={`${classes.headerMobile_left} hidden md:flex`}>
+        <div className={`${classes.headerMobile_left} hidden lg:flex`}>
           <a href="">
             <Button
               className="bg-white !text-blue-500 h-full transition hover:bg-blue-700 hover:!text-white"
